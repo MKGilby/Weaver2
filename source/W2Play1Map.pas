6369679497f7f8ba4cefb902d0c4f85c8bfded25
HUNGilby <gilby@mksztsz.hu>
2025-01-20 21:03:38 +0100

    V0.1 build 1
    - I got stuck drawing colored background tiles using current two color style, so starting over, using full colors for gfx. In this commit I comment out or remove all code and graphics associated with two colored version.
    - Border of game area is gray.
    - Floor, wall and zapper background color is set in map json file.

--- source/W2Play1Map.pas
+++ source/W2Play1Map.pas
@@ -30,7 +30,7 @@ type
 implementation
 
 uses
-  sdl2, ARGBImageUnit, W2Shared;
+  sdl2, ARGBImageUnit, W2Shared, MKStream, Math;
 
 const
   // Put these in the order of tiles in 'tiles.png'
@@ -47,7 +47,7 @@ begin
   Entities:=TMapEntities.Create;
   CreateBack;
   BlockCount:=0;
-  for y:=-1 to MAPHEIGHT do
+{  for y:=-1 to MAPHEIGHT do
     for x:=-1 to MAPWIDTH do begin
       case fMap.OrigTiles[x,y] of
         LOADED_TILE_FLOOR:fMap.Tiles[x,y]:=TILE_FLOOR;
@@ -160,7 +160,7 @@ begin
   fPlayer:=TPlayer.Create(fMap);
   Entities.Add(fPlayer);
   for x:=0 to fMap.MonsterCount-1 do
-    Entities.Add(TMonster.Create(fMap,x));
+    Entities.Add(TMonster.Create(fMap,x));}
 end;
 
 destructor TPlay1Map.Destroy;
@@ -179,29 +179,34 @@ begin
   pre:=GetTickCount64;
   repeat
     now:=GetTickCount64;
-    Entities.Move((now-pre)/1000);
+//    Entities.Move((now-pre)/1000);
     pre:=now;
     SDL_SetRenderDrawColor(PrimaryWindow.Renderer,0,0,0,255);
     SDL_RenderClear(PrimaryWindow.Renderer);
-    if Entities.IsPlayerCollidedWithEnemies(fPlayer.CollisionData) then
-      MM.Fonts.OutText('COLLIDE!',568,0,1);
-    MM.Fonts.OutText(#1'BLOCKS: '#0+inttostr(BlockCount),568,128,1);
+//    if Entities.IsPlayerCollidedWithEnemies(fPlayer.CollisionData) then
+//      MM.Fonts.OutText('COLLIDE!',568,0,1);
+//    MM.Fonts.OutText(#1'BLOCKS: '#0+inttostr(BlockCount),568,128,1);
 
     PutTexture(MAPLEFT-8,MAPTOP-8,fBack);
-    Entities.Draw;
+//    Entities.Draw;
 
     FlipNoLimit;
     HandleMessages;
     if keys[SDL_SCANCODE_ESCAPE] then Result:=-1;
     if controllerbuttons[SDL_CONTROLLER_BUTTON_X] then Result:=-1;
     if Terminate then Result:=-1;
-    if fPlayer.State=psExit then Result:=1;
+//    if fPlayer.State=psExit then Result:=1;
   until Result<>0;
 
 end;
 
 procedure TPlay1Map.CreateBack;
+const
+  DARK=$FF303030;
+  MID=$FF585858;
+  BRIGHT=$FF808080;
 var tmpI,tmpT:TARGBImage;i,j,ti:integer;s:string;
+    floor,zapper,wall:TARGBImage;
 
   function IndexOfTile(pName:string):integer;
   var i:integer;
@@ -214,50 +219,97 @@ var tmpI,tmpT:TARGBImage;i,j,ti:integer;s:string;
       end;
   end;
 
-begin
-  tmpT:=MM.Images.ItemByName['Tiles'];
-  if Assigned(tmpT) then begin
-    tmpI:=TARGBImage.Create(MAPWIDTH*TILESIZE+16,MAPHEIGHT*TILESIZE+16);
+  function CreateFromBIN(const pFilename:string):TARGBImage;
+  var grmap:pointer;Xs:TStream;x,y:integer;
+  begin
+    Result:=TARGBImage.Create(TILESIZE,TILESIZE);
+    grmap:=getmem(TILESIZE*TILESIZE);
     try
-      tmpI.Clear;
-      for j:=0 to MAPHEIGHT-1 do
-        for i:=0 to MAPWIDTH-1 do begin
-          case fMap.OrigTiles[i,j] of
-            LOADED_TILE_ZAPPER1,LOADED_TILE_ZAPPER2,LOADED_TILE_ZAPPER3,LOADED_TILE_ZAPPER4:s:='Zapper';
-            LOADED_TILE_WALL:s:='Wall';
-            LOADED_TILE_COLOR1:s:='Color1';
-            LOADED_TILE_COLOR2:s:='Color2';
-            LOADED_TILE_COLOR3:s:='Color3';
-            LOADED_TILE_DOORBUTTON1:s:='Button1';
-            LOADED_TILE_DOORBUTTON2:s:='Button2';
-            LOADED_TILE_DOORBUTTON3:s:='Button3';
-            LOADED_TILE_DOORBUTTON4:s:='Button4';
-            LOADED_TILE_DOORBUTTON5:s:='Button5';
-            LOADED_TILE_DOORBUTTON6:s:='Button6';
-            else s:='Floor';
-          end;
-          ti:=IndexOfTile(s)*TILESIZE;
-          if (ti>-1) then
-            tmpI.PutImagePart(i*TILESIZE+8,j*TILESIZE+8,ti mod tmpT.Width,ti div tmpT.Width*TILESIZE,TILESIZE,TILESIZE,tmpT);
-        end;
-      tmpI.HLine(0,0,tmpI.Width,MonoColor32);
-      tmpI.HLine(1,1,tmpI.Width-2,MonoColor32);
-      tmpI.VLine(tmpI.Width-1,0,tmpI.Height,MonoColor32);
-      tmpI.VLine(tmpI.Width-2,1,tmpI.Height-2,MonoColor32);
-
-      tmpI.VLine(5,6,tmpI.Height-10,MonoColor32);
-      tmpI.VLine(6,7,tmpI.Height-12,MonoColor32);
-      tmpI.HLine(5,tmpI.Height-6,tmpI.Width-11,MonoColor32);
-      tmpI.HLine(6,tmpI.Height-7,tmpI.Width-13,MonoColor32);
-      for i:=2 to tmpI.Width-3 do
-        for j:=2 to tmpI.Height-3 do
-          if ((i<5) or (i>tmpI.Width-6)) or ((j<5) or (j>tmpI.Height-6)) then
-            if (i+j) mod 2=0 then tmpI.PutPixel(i,j,MonoColor32);
-      fBack:=TStaticTexture.Create(tmpI);
+      Xs:=MKStreamOpener.OpenStream(pFilename+'.bin');
+      try
+        Xs.Read(grmap^,min(Xs.Size,TILESIZE*TILESIZE));
+      finally
+        Xs.Free;
+      end;
+      for y:=0 to TILESIZE-1 do
+        for x:=0 to TILESIZE-1 do
+          Result.PutPixel(x,y,fMap.Gradient.GetColorAt(byte((grmap+y*TILESIZE+x)^)/256));
     finally
-      tmpI.Free;
+      freemem(grmap);
     end;
   end;
+
+begin
+//  tmpT:=MM.Images.ItemByName['Tiles'];
+//  if Assigned(tmpT) then begin
+    floor:=CreateFromBIN('floor');
+    zapper:=CreateFromBIN('zapper_floor');
+    wall:=CreateFromBIN('wall');
+    try
+      tmpI:=TARGBImage.Create(MAPWIDTH*TILESIZE+16,MAPHEIGHT*TILESIZE+16);
+      try
+        tmpI.Clear;
+        for j:=0 to MAPHEIGHT-1 do
+          for i:=0 to MAPWIDTH-1 do begin
+            tmpI.PutImage(i*TILESIZE+8,j*TILESIZE+8,floor);
+            case fMap.OrigTiles[i,j] of
+              LOADED_TILE_ZAPPER1,LOADED_TILE_ZAPPER2,LOADED_TILE_ZAPPER3,LOADED_TILE_ZAPPER4:begin
+                s:='';
+                tmpI.PutImage(i*TILESIZE+8,j*TILESIZE+8,zapper);
+              end;
+              LOADED_TILE_WALL:begin
+                s:='';
+                tmpI.PutImage(i*TILESIZE+8,j*TILESIZE+8,wall);
+              end;
+              LOADED_TILE_COLOR1:s:='Color1';
+              LOADED_TILE_COLOR2:s:='Color2';
+              LOADED_TILE_COLOR3:s:='Color3';
+              LOADED_TILE_DOORBUTTON1:s:='Button1';
+              LOADED_TILE_DOORBUTTON2:s:='Button2';
+              LOADED_TILE_DOORBUTTON3:s:='Button3';
+              LOADED_TILE_DOORBUTTON4:s:='Button4';
+              LOADED_TILE_DOORBUTTON5:s:='Button5';
+              LOADED_TILE_DOORBUTTON6:s:='Button6';
+              otherwise begin
+                s:='';
+              end;
+            end;
+{            ti:=IndexOfTile(s)*TILESIZE;
+            if (ti>-1) then
+              tmpI.PutImagePart(i*TILESIZE+8,j*TILESIZE+8,ti mod tmpT.Width,ti div tmpT.Width*TILESIZE,TILESIZE,TILESIZE,tmpT);
+}
+          end;
+        tmpI.HLine(0,0,tmpI.Width,BRIGHT);
+        tmpI.HLine(1,1,tmpI.Width-2,BRIGHT);
+        tmpI.VLine(0,1,tmpI.Height-1,DARK);
+        tmpI.VLine(1,2,tmpI.Height-3,DARK);
+        tmpI.VLine(tmpI.Width-1,0,tmpI.Height,BRIGHT);
+        tmpI.VLine(tmpI.Width-2,1,tmpI.Height-2,BRIGHT);
+        tmpI.HLine(0,tmpI.Height-1,tmpI.Width-1,DARK);
+        tmpI.HLine(1,tmpI.Height-2,tmpI.Width-3,DARK);
+
+        tmpI.VLine(5,6,tmpI.Height-10,BRIGHT);
+        tmpI.VLine(6,7,tmpI.Height-12,BRIGHT);
+        tmpI.HLine(5,5,tmpI.Width-10,DARK);
+        tmpI.HLine(6,6,tmpI.Width-12,DARK);
+
+        tmpI.HLine(5,tmpI.Height-6,tmpI.Width-11,BRIGHT);
+        tmpI.HLine(6,tmpI.Height-7,tmpI.Width-13,BRIGHT);
+        tmpI.VLine(tmpI.Width-6,5,tmpI.Height-11,DARK);
+        tmpI.VLine(tmpI.Width-7,6,tmpI.Height-13,DARK);
+        tmpI.Rectangle(2,2,tmpI.Width-4,tmpI.Height-4,MID);
+        tmpI.Rectangle(3,3,tmpI.Width-6,tmpI.Height-6,MID);
+        tmpI.Rectangle(4,4,tmpI.Width-8,tmpI.Height-8,MID);
+        fBack:=TStaticTexture.Create(tmpI);
+      finally
+        tmpI.Free;
+      end;
+    finally
+      floor.Free;
+      zapper.Free;
+      wall.Free;
+    end;
+//  end;
 end;
 
 end.
--- work/TilesAndSprites/run.bat
+++ work/TilesAndSprites/run.bat
@@ -4,5 +4,11 @@ echo This script prepares tiles and sprites and copies them to data folder.
 echo ----------------------------------------------------------------------
 
 ..\..\tools\mkconv2 convert.mc2
+
+..\..\tools\cut floor.gsd 4 1024 ..\..\data\floor.bin
+..\..\tools\cut zapper_floor.gsd 4 1024 ..\..\data\zapper_floor.bin
+..\..\tools\cut wall.gsd 4 1024 ..\..\data\wall.bin
+del *.gsd
+
 for %%i in (*.png) do ..\..\tools\pngout %%i ..\..\data\%%i /y /kanMZ,fnTZ,anIM /f0
 del *.png
